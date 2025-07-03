f756329e8c236db3e79758a1c55794842ceed504
Anja Gjerpe <anjamgjerpe@gmail.com>
2025-07-03 10:47:43 +0200

    Add Ticket model

--- frontend/src/main/scala/exp/model/DataModel.scala
+++ frontend/src/main/scala/exp/model/DataModel.scala
@@ -5,5 +5,5 @@ import upickle.default.{ReadWriter, macroRW}
 final case class Ticket(id: String, title: String, description: String)
 
 object Ticket {
-  given rw: ReadWriter[Ticket] = macroRW[Ticket]
+  // given rw: ReadWriter[Ticket] = macroRW[Ticket]
 }
